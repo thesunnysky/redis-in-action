761d12e05d0e9f536457a4ce4542f7ce707f6af0
thesunnysky <jianyungsun@gmail.com>
2020-03-14 16:37:36 +0800

    update sentinels

--- sentinel/start.sh
+++ sentinel/start.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
-BASE_DIR=`pwd`
+BASE_DIR="."
 #echo $BASE_DIR
 
+rm -rf data/*
+
 cnt=0
-for conf_file in $BASE_DIR/redis-server-*.conf
+for conf_file in $BASE_DIR/server-*.conf
 do 
     echo "start redis server:" $conf_file
     redis-server $conf_file
 	let cnt++
 done
+
 echo "${cnt} redis server started"
 ps -ef | grep redis-server | grep -v "grep" | grep -v "sentinel"
 
 echo -e "\nBegein to start redis-sentinel..."
 cnt=0
-for conf_file in $BASE_DIR/redis-sentinel-*.conf
+for conf_file in $BASE_DIR/sentinel-*.conf
 do 
-    echo "start redis-sentinel:" $conf_file
+    echo "start sentinel:" $conf_file
     redis-server $conf_file --sentinel
 	let cnt++
 done
-echo "${cnt} redis sentinel started"
+echo "${cnt} sentinel started"
 ps -ef | grep redis-server | grep -v "grep" | grep sentinel
 
+for i in {26379..26381}
+do
+    redis-cli -p $i sentinel monitor master1 127.0.0.1 6379 2
+done
+
+echo "127.0.0.1:6380 slaveof 127.0.0.1:6379"
+redis-cli -p 6380 slaveof 127.0.0.1 6379
